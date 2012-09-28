4379dff72373c2d60a90792053704365a1f51a0d
Mohamed Bassem <medoox240@gmail.com>
2012-09-28 23:06:22 +0200

    editing index

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 
 session_start();
 
-$dhost = "localhost"; // DB Server
-$duser = "root"; // DB User
-$dpass = ""; // DB Password
-$dbname = ""; // DB Name
+//$dhost = "localhost"; // DB Server
+//$duser = "root"; // DB User
+//$dpass = ""; // DB Password
+//$dbname = ""; // DB Name
 
-$con = @mysql_connect($dhost,$duser,$dpass);
-$selectdb = @mysql_select_db($dbname,$con);
+//$con = @mysql_connect($dhost,$duser,$dpass);
+//$selectdb = @mysql_select_db($dbname,$con);
 
 $type = $_GET['s'];
 include('header.php');
